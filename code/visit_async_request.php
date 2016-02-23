f216e1fb2db5874a01d1f5cb28d854f6222e37ce
Josh T <timmer.jo+git@gmail.com>
2016-02-23 10:32:27 -0700

    Updated WHERE building

--- code/visit_async_request.php
+++ code/visit_async_request.php
@@ -3,39 +3,53 @@
 require_once('lib/helpers/visits-setup.inc.php');
 $query;
 if(isset($_REQUEST['brand']) && !empty($_REQUEST['brand'])) {
-    if(empty($query)){
-        $query = 'WHERE ';
+    if(!isset($query)){
+        $query = 'WHERE dt.name = \''.$_REQUEST['brand'].'\' ';
     }
-
-    $query.'dt.name = \''.$_REQUEST['brand'].'\' ';
+    else
+    {
+        $query.'dt.name = \''.$_REQUEST['brand'].'\' ';
+    }
 }
 if(isset($_REQUEST['referrer']) && !empty($_REQUEST['referrer'])){
-    if(empty($query)){
-        $query = 'WHERE ';
+    if(!isset($query)){
+        $query = 'WHERE referrer_id = \''.$_REQUEST['referrer'].'\' ';
+    }
+    else
+    {
+        $query.'AND referrer_id = \''.$_REQUEST['referrer'].'\' ';
     }
 
-    $query.'referrer_id = \''.$_REQUEST['referrer'].'\' ';
 }
 if(isset($_REQUEST['os']) && !empty($_REQUEST['os'])) {
-    if(empty($query)){
-        $query = 'WHERE ';
+    if(!isset($query)){
+        $query = 'WHERE os_id = \''.$_REQUEST['os'].'\' ';
+    }
+    else
+    {
+        $query.'AND os_id = \''.$_REQUEST['os'].'\' ';
     }
 
-    $query.'os_id = \''.$_REQUEST['os'].'\' ';
 }
 if(isset($_REQUEST['dt']) && !empty($_REQUEST['dt'])) {
-    if(empty($query)){
-        $query = 'WHERE ';
-    }
 
-    $query.'device_type_id = \''.$_REQUEST['dt'].'\' ';
+    if(!isset($query)){
+        $query = 'WHERE device_type_id = \''.$_REQUEST['dt'].'\' ';
+    }
+    else
+    {
+        $query.'AND device_type_id = \''.$_REQUEST['dt'].'\' ';
+    }
 }
 if(isset($_REQUEST['browser']) && !empty($_REQUEST['browser'])){
-    if(empty($query)){
-        $query = 'WHERE ';
+    if(!isset($query)){
+        $query = 'WHERE browser_id = \''.$_REQUEST['browser'].'\' ';
+    }
+    else
+    {
+        $query.' AND browser_id = \''.$_REQUEST['browser'].'\' ';
     }
 
-    $query.'browser_id = \''.$_REQUEST['browser'].'\' ';
 }
 //    $passedValue = $_REQUEST['brand'];
 //if(strcmp($query,"WHERE ") == 0)
