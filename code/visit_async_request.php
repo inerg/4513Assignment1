828f5a4f1862d92559c19524ee7cf5f12a0bf6c8
Josh T <timmer.jo+git@gmail.com>
2016-02-23 10:04:19 -0700

    Async page should handle query where modifier

--- code/visit_async_request.php
+++ code/visit_async_request.php
@@ -1,12 +1,26 @@
 <?php
 
 require_once('lib/helpers/visits-setup.inc.php');
-
-//if(isset($_REQUEST['brand']) && !empty($_REQUEST['brand'])) {
+$query = 'WHERE ';
+if(isset($_REQUEST['brand']) && !empty($_REQUEST['brand'])) {
+    $query.'dt.name = \''.$_REQUEST['brand'].'\' ';
+}
+if(isset($_REQUEST['referrer']) && !empty($_REQUEST['referrer'])){
+    $query.'referrer_id = \''.$_REQUEST['referrer'].'\' ';
+}
+if(isset($_REQUEST['os']) && !empty($_REQUEST['os'])) {
+    $query.'os_id = \''.$_REQUEST['os'].'\' ';
+}
+if(isset($_REQUEST['dt']) && !empty($_REQUEST['dt'])) {
+    $query.'device_type_id = \''.$_REQUEST['dt'].'\'';
+}
+if(isset($_REQUEST['browser']) && !empty($_REQUEST['browser'])){
+    $query.'browser_id = \''.$_REQUEST['browser'].'\'';
+}
 //    $passedValue = $_REQUEST['brand'];
     
 	$gate = new VisitsBrowserTableGateway($dbAdapter);
-	$result = $gate->getVisitInfo($passedValue);
+	$result = $gate->getVisitInfo($query);
 
 	echo json_encode($result);
 ?>
