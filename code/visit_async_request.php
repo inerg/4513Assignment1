468116c6a06ad5d1ac6f59260a1f75287af6db8a
Josh T <timmer.jo+git@gmail.com>
2016-02-23 10:26:38 -0700

    PHP sucks at strings.

--- code/visit_async_request.php
+++ code/visit_async_request.php
@@ -1,27 +1,47 @@
 <?php
 
 require_once('lib/helpers/visits-setup.inc.php');
-$query = 'WHERE ';
+$query;
 if(isset($_REQUEST['brand']) && !empty($_REQUEST['brand'])) {
+    if(empty($query)){
+        $query = 'WHERE ';
+    }
+
     $query.'dt.name = \''.$_REQUEST['brand'].'\' ';
 }
 if(isset($_REQUEST['referrer']) && !empty($_REQUEST['referrer'])){
+    if(empty($query)){
+        $query = 'WHERE ';
+    }
+
     $query.'referrer_id = \''.$_REQUEST['referrer'].'\' ';
 }
 if(isset($_REQUEST['os']) && !empty($_REQUEST['os'])) {
+    if(empty($query)){
+        $query = 'WHERE ';
+    }
+
     $query.'os_id = \''.$_REQUEST['os'].'\' ';
 }
 if(isset($_REQUEST['dt']) && !empty($_REQUEST['dt'])) {
+    if(empty($query)){
+        $query = 'WHERE ';
+    }
+
     $query.'device_type_id = \''.$_REQUEST['dt'].'\' ';
 }
 if(isset($_REQUEST['browser']) && !empty($_REQUEST['browser'])){
+    if(empty($query)){
+        $query = 'WHERE ';
+    }
+
     $query.'browser_id = \''.$_REQUEST['browser'].'\' ';
 }
 //    $passedValue = $_REQUEST['brand'];
-if(strcmp($query,"WHERE ") == 0)
-{
-    $query = "";
-}
+//if(strcmp($query,"WHERE ") == 0)
+//{
+//    $query = "";
+//}
     
 	$gate = new VisitsBrowserTableGateway($dbAdapter);
 	$result = $gate->getVisitInfo($query);
