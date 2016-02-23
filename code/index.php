31dc620a78d435aa9c69e9c66dcb248579067d5e
Bradley Biederman <Bradley Biederman>
2016-02-23 15:19:34 -0700

    Fixed device dropdown in index.php

--- code/index.php
+++ code/index.php
@@ -22,16 +22,23 @@
                       <div class="row">
                           <div class="col s11">
                               <form action="index.php" method="get" id="brandSelect" target="_self">
-                                      <?php
+								<div class="input-field col s3">
+							  <select  class="btn teal lighten-2 dropdown-button-widths" name="brand" onchange="formChange('brandSelect')">
+							  <?php
                                         $deviceBrandGate->printBrandDropdown(trim($_GET['brand']));
-                                      ?>
-                              </form>
+                               ?>
+                              </select>
+							  </div>
+							  </form>
+							  
+							  
                               <?php
                                 if(ISSET($_GET['brand']))
 								{
 									$deviceBrandGate->printBrandVists(trim($_GET['brand']));
 								}
 							  ?>
+							  
                           </div>
                       </div>
 				  </div>
--- code/lib/model/DeviceBrandTableGateway.class.php
+++ code/lib/model/DeviceBrandTableGateway.class.php
@@ -61,8 +61,6 @@ class DeviceBrandTableGateway extends TableDataGateway
     //Outputs a option list of brands.
     public function printBrandDropdown($selectedBrand = null)
     {
-        echo '<div class="input-field col s3">';
-        echo '<select  class="btn teal lighten-2 dropdown-button-widths" id="brand">';
         if($selectedBrand == null) {
             echo '<option class="placeholder white-text teal darken-1" selected value="false">Select Brand</option>';
         } else {
@@ -77,8 +75,6 @@ class DeviceBrandTableGateway extends TableDataGateway
                 echo '<option value="' . $brand['name'] . '">' . $brand['name'] . '</option>';
             }
         }
-        echo '</select>';
-        echo '</div>';
 
     }
     //Prints the vists for the brand selected.
